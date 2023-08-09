d0d16bf389c1703b5e960d8742856b5d3101a888
Alex Cruikshank <acruikshank@westmonroe.com>
2023-08-09 09:24:26 -0400

    remove unused code

--- scripts/pickle_to_json.py
+++ scripts/pickle_to_json.py
@@ -21,29 +21,4 @@ def main(args):
 
     print(df_emb.to_json())
 
-  
-  
-
-
-
-
-
-    # top_hits = retrieve_relevant_answer(input_question, df_emb, num_hits, embed_model, embed_encoder, max_tokens)
-
-    # if not json_output_flag:
-    #     # retrieve best question and answer TODO: Make it so we can retrieve many examples
-    #     top_question = top_hits["questions"].iloc[0]
-    #     top_answer = top_hits["answers"].iloc[0]
-
-    #     # Create question prompt
-    #     prompt = question_boiler.format(top_question, top_answer, input_question)
-
-    #     ans = run_gpt(openai_model, system_boiler, prompt, temp, max_tokens, max_tokens_output)
-    #     return print(ans)
-    # else:
-    #     if output_file_location:
-    #         return top_hits.head(num_hits).to_json(output_file_location)
-    #     else:
-    #         return(print(top_hits.head(num_hits).to_json()))
-
 main(args)
