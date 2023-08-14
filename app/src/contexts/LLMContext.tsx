512003a1837b3335c5e4db8687fd1e5d39d59098
wmjrosenblatt <131193295+wmjrosenblatt@users.noreply.github.com>
2023-08-14 12:06:54 -0500

    The comparison of the dirtyLLMs was not working when keys were in different orders (#56)
    * The comparison of the dirtyLLMs and the llms were being compared in the wrong way
    
    * adding comparison for when object values are arrays

--- app/src/contexts/LLMContext.tsx
+++ app/src/contexts/LLMContext.tsx
@@ -30,6 +30,47 @@ export interface LLMProviderProps {
   children: React.ReactNode;
 }
 
+const areLLMDirtyLLMEqual = (llms: Record<string, any>, dirtyLLMs: Record<string, any>): boolean => {
+  const keys1 = Object.keys(llms);
+  const keys2 = Object.keys(dirtyLLMs);
+  
+  if (keys1.length !== keys2.length) {
+      return false;
+  }
+
+  return keys1.every(key => {
+      if (!(key in dirtyLLMs)) {
+          return false;
+      }
+
+      // compare array values
+      if (Array.isArray(llms[key]) && Array.isArray(dirtyLLMs[key])) {
+        if (llms[key].length !== dirtyLLMs[key].length) {
+          return false;
+        }
+
+        for (let i = 0; i < llms[key].length; i++) {
+          if (typeof llms[key][i] === 'object' && llms[key][i] !== null) {
+            if (!areLLMDirtyLLMEqual(llms[key][i], dirtyLLMs[key][i])) {
+              return false;
+            }
+          } else if (llms[key][i] !== dirtyLLMs[key][i]) {
+            return false;
+          }
+        }
+        return true;
+      }
+      // compare object values
+      else if (typeof llms[key] === 'object' && llms[key] !== null && typeof dirtyLLMs[key] === 'object' && dirtyLLMs[key] !== null) {
+          return areLLMDirtyLLMEqual(llms[key], dirtyLLMs[key]);
+      }
+      // compare primitives
+      else {
+          return llms[key] === dirtyLLMs[key];
+      }
+  });
+}
+
 export const LLMContextProvider: React.FC<LLMProviderProps> = ({ children }) => {
   const [llms, setLLMs] = useState<Record<string, LLM>>(defaultLLMs);
   const [dirtyLLMs, setDirtyLLMs] = useState<Record<string, LLM>>({});
@@ -51,7 +92,7 @@ export const LLMContextProvider: React.FC<LLMProviderProps> = ({ children }) => 
   }
 
   useEffect(() => {
-    setLLMsNeedSave(JSON.stringify(llms) !== JSON.stringify(dirtyLLMs));
+    setLLMsNeedSave(!areLLMDirtyLLMEqual(llms, dirtyLLMs));
   }, [llms, dirtyLLMs]);
 
   const deleteLLM = (name: string): void => {
